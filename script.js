51130be316d794fabc53befdc2d187a6af019903
Forcowicz <forcowicz@gmail.com>
2020-12-20 04:57:12 +0100

    Refactoring

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Selecting all necessary elements
 const score0Element = document.getElementById('score--0');
 const score1Element = document.getElementById('score--1');
 const current0Element = document.getElementById('current--0')
@@ -12,31 +11,18 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Application-state variables
-const scores = [0, 0];
-let score = 0;
-let activePlayer = 0;
-let gameOver = false;
+let gameOver, activePlayer, score, scores;
 
-// Starting conditions
-score0Element.textContent = '0';
-score1Element.textContent = '0';
-diceElement.classList.add('hidden');
+init();
 
-// Rolling dice
 btnRoll.addEventListener('click', () => {
     if(!gameOver) {
-        // 1. Generating a random dice roll
         const dice = Math.trunc(Math.random() * 6) + 1;
-
-        // 2. Display the dice
         diceElement.classList.remove('hidden');
         diceElement.src = `dice-${dice}.png`;
-
-        // 3. Check for rolled 1: if true switch to next player
         if (dice !== 1) {
             score += dice;
-            setCurrentScore(score); // 0 or 1
+            setCurrentScore(score);
         } else {
             switchPlayer();
         }
@@ -61,22 +47,25 @@ btnHold.addEventListener('click', () => {
    }
 });
 
-btnNew.addEventListener('click', () => {
-    gameOver = false;
-    scores[0] = 0; scores[1] = 1;
-    activePlayer = 0;
+btnNew.addEventListener('click', init);
+
+function init() {
+    scores = [0, 0];
     score = 0;
+    activePlayer = 0;
+    gameOver = false;
+    score0Element.textContent = '0';
+    score1Element.textContent = '0';
     current0Element.textContent = 0;
     current1Element.textContent = 0;
+    diceElement.classList.add('hidden');
     document.querySelector('.player--0').className = 'player player--0 player--active';
     document.querySelector('.player--1').className = 'player player--1';
-    score0Element.textContent = 0;
-    score1Element.textContent = 0;
     document.getElementById('name--0').textContent = 'Player 1';
     document.getElementById('name--1').textContent = 'Player 2';
     btnRoll.classList.remove('hidden');
     btnHold.classList.remove('hidden');
-});
+}
 
 function setCurrentScore(score) {
     document.getElementById(`current--${activePlayer}`).textContent = score;
